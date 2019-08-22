cb44e619d8749958e4da1e105540db5c89d5e4dd
Sudeep Bhandari <sudeephb19@gmail.com>
2019-08-22 17:02:31 +0545

    Update apiviews.py

--- data_entry/apiviews.py
+++ data_entry/apiviews.py
@@ -82,18 +82,29 @@ def get_syllabus_program_year(program,year):
 
 
 def get_syllabus_year(year):
-    program_short_name = 'BCT'
-    id = Program.objects.get(program_short_name = program_short_name).program_id
-    syllabus_1 = Syllabus.objects.get(year = year ,part = 1, program_id=id)
-    syllabus_2 = Syllabus.objects.get(year = year ,part = 2, program_id=id)
-    bct_year_syllabus = [syllabus_1, syllabus_2]
-    program_short_name = 'BEX'
-    id = Program.objects.get(program_short_name = program_short_name).program_id
-    syllabus_1 = Syllabus.objects.get(year = year ,part = 1, program_id=id)
-    syllabus_2 = Syllabus.objects.get(year = year ,part = 2, program_id=id)
-    bex_year_syllabus = [syllabus_1, syllabus_2]
+    programs = list(Program.objects.all())
+    res = []
+    for prog in programs:
+        id = prog.program_id
+        syllabus_1 = Syllabus.objects.get(year = year ,part = 1, program_id=id)
+        syllabus_2 = Syllabus.objects.get(year = year ,part = 2, program_id=id)
+        res.append([syllabus_1, syllabus_2])
     
-    return [bct_year_syllabus, bex_year_syllabus]
+    return res
+    
+    
+#     program_short_name = 'BCT'
+#     id = Program.objects.get(program_short_name = program_short_name).program_id
+#     syllabus_1 = Syllabus.objects.get(year = year ,part = 1, program_id=id)
+#     syllabus_2 = Syllabus.objects.get(year = year ,part = 2, program_id=id)
+#     bct_year_syllabus = [syllabus_1, syllabus_2]
+#     program_short_name = 'BEX'
+#     id = Program.objects.get(program_short_name = program_short_name).program_id
+#     syllabus_1 = Syllabus.objects.get(year = year ,part = 1, program_id=id)
+#     syllabus_2 = Syllabus.objects.get(year = year ,part = 2, program_id=id)
+#     bex_year_syllabus = [syllabus_1, syllabus_2]
+    
+#     return [bct_year_syllabus, bex_year_syllabus]
 
 def get_syllabus_program(program):
     #uncomment these lines when data of all 4 years are available
